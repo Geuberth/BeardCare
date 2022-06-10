ca750a5abfbe7d03de2715b76651a1b2d133ae06
Geuberth <g.robles@utp.edu.co>
2022-06-10 01:19:38 -0500

    Login added Choose added logo added fonts added

---
+++ lib/animatedbutton.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+
+class NeuBotton extends StatelessWidget {
+  final onTap;
+  final onTap1;
+  final redirectP;
+  final redirectB;
+  bool isPressed;
+  bool isPressed1;
+
+  NeuBotton(
+      {this.redirectB,
+      this.redirectP,
+      this.onTap1,
+      this.onTap,
+      required this.isPressed,
+      required this.isPressed1});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: onTap,
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 200),
+            height: 180,
+            width: 180,
+            onEnd: redirectP,
+            decoration: BoxDecoration(
+                color: isPressed ? Colors.grey[400] : Colors.black87,
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: isPressed
+                    ? [
+                        //No shadows if the button is pressed
+                      ]
+                    : [
+                        //Dark shadow at bottom right
+                        BoxShadow(
+                          color: Colors.grey.shade500,
+                          offset: Offset(6, 6),
+                          blurRadius: 9,
+                          spreadRadius: 1,
+                        ),
+
+                        //Light Shadow at top left
+                        BoxShadow(
+                          color: Colors.white,
+                          offset: Offset(-6, -6),
+                          blurRadius: 9,
+                          spreadRadius: 1,
+                        ),
+                      ]),
+            child: Column(
+              children: [
+                SizedBox(height: 8,),
+                Icon(
+                  Icons.person,
+                  size: 130,
+                  color: isPressed ? Colors.grey[400] : Colors.amber,
+                ),
+                SizedBox(height: 5,),
+                Text(
+                  'Cliente',
+                  style: TextStyle(
+                      color: isPressed ? Colors.grey[400] : Colors.amber,
+                      fontFamily: 'CrimsonText',
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        GestureDetector(
+          onTap: onTap1,
+          child: AnimatedContainer(
+              duration: Duration(milliseconds: 200),
+              height: 180,
+              width: 180,
+              onEnd: redirectB,
+              decoration: BoxDecoration(
+                  color: isPressed1 ? Colors.grey[400] : Colors.black87,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: isPressed1
+                      ? [
+                          //No shadows
+                        ]
+                      : [
+                          //Dark shadow at bottom right
+                          BoxShadow(
+                            color: Colors.grey.shade500,
+                            offset: Offset(6, 6),
+                            blurRadius: 9,
+                            spreadRadius: 1,
+                          ),
+
+                          //Light Shadow at top left
+                          BoxShadow(
+                            color: Colors.white,
+                            offset: Offset(-6, -6),
+                            blurRadius: 9,
+                            spreadRadius: 1,
+                          ),
+                        ]),
+              child: Column(
+                children: [
+                  SizedBox(height: 8,),
+                  Icon(
+                    Icons.assignment_ind,
+                    size: 130,
+                    color: isPressed1 ? Colors.grey[400] : Colors.amber,
+                  ),
+                  SizedBox(height: 5,),
+                  Text(
+                    'Barberia',
+                    style: TextStyle(
+                        color: isPressed ? Colors.grey[400] : Colors.amber,
+                        fontFamily: 'CrimsonText',
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              )),
+        ),
+      ],
+    );
+  }
+/*      children: [
+        GestureDetector(
+          onTap: onTap,
+          child: Container(
+            height: 160,
+            width: 160,
+            decoration: BoxDecoration(
+                color: Colors.grey[150],
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  //Dark shadow at bottom right
+                  BoxShadow(
+                    color: Colors.grey.shade500,
+                    offset: Offset(6, 6),
+                    blurRadius: 15,
+                    spreadRadius: 1,
+                  ),
+
+                  //Light Shadow at top left
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(6, 6),
+                    blurRadius: 15,
+                    spreadRadius: 1,
+                  ),
+                ]),
+            child: const Icon(
+              Icons.assignment_ind,
+              size: 60,
+              color: Colors.amber,
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        GestureDetector(
+          onTap: onTap,
+          child: Container(
+            height: 160,
+            width: 160,
+            decoration: BoxDecoration(
+                color: Colors.grey[150],
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  //Dark shadow at bottom right
+                  BoxShadow(
+                    color: Colors.grey.shade500,
+                    offset: Offset(6, 6),
+                    blurRadius: 15,
+                    spreadRadius: 1,
+                  ),
+
+                  //Light Shadow at top left
+                  BoxShadow(
+                    color: Colors.white,
+                    offset: Offset(6, 6),
+                    blurRadius: 15,
+                    spreadRadius: 1,
+                  ),
+                ]),
+            child: const Icon(
+              Icons.assignment_ind,
+              size: 60,
+              color: Colors.amber,
+            ),
+          ),
+        ),
+      ],*/
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'animatedbutton.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +11,185 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Beard&Care',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.amber,
+        //colorScheme: ColorScheme(primary: ,secondary: ,error: ,background: ,brightness: ,onPrimary: ,onSecondary: ,onSurface: ,onError: ),
       ),
-      home: const MyHomePage(title: 'Beard&Care'),
+      initialRoute: ChooseScreen.id,
+      routes: {
+        LoginScreen.id: (context) => LoginScreen(),
+        ChooseScreen.id: (context) => ChooseScreen()
+      },
+      home: const LoginScreen(),
     );
+  }
+}
+
+class ChooseScreen extends StatefulWidget {
+  static String id = 'Choose';
+  const ChooseScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChooseScreen> createState() => _ChooseScreenState();
+}
+
+class _ChooseScreenState extends State<ChooseScreen> {
+  @override
+  bool isPressed = false;
+  bool isPressed1 = false;
+  void buttonPressed() {
+    setState(() {
+      isPressed = true;
+    });
+  }
+
+  void buttonPressed1() {
+    setState(() {
+      if (isPressed1 == false) {
+        isPressed1 = true;
+      } else if (isPressed1 == true) {
+        isPressed1 = false;
+      }
+    });
+  }
+
+  void redirectPerson() {
+    if (isPressed == true) {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const LoginScreen()));
+    }
+    setState(() {
+      isPressed = false;
+    });
+  }
+
+  void redirectBarber() {
+    if (isPressed1 == true) {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const LoginScreen()));
+    }
+    setState(() {
+      isPressed1 = false;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.grey[400],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              NeuBotton(
+                onTap: buttonPressed,
+                onTap1: buttonPressed1,
+                redirectP: redirectPerson,
+                redirectB: redirectBarber,
+                isPressed: isPressed,
+                isPressed1: isPressed1,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// definition of the widget Login screen
+class LoginScreen extends StatefulWidget {
+  static String id = 'Login';
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+//Login screen
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'Assets/beardL.png',
+                height: 300,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              userTextField(),
+              SizedBox(
+                height: 6,
+              ),
+              passwordTextField(),
+              SizedBox(
+                height: 20,
+              ),
+              bottonLogin(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget userTextField() {
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 50),
+        child: const TextField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+            icon: Icon(Icons.email),
+            hintText: 'Example@gmail.com',
+            labelText: 'Email',
+          ),
+/*              onChanged: (value){
+              },*/
+        ),
+      );
+    });
+  }
+
+  Widget passwordTextField() {
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 50),
+        child: const TextField(
+          keyboardType: TextInputType.visiblePassword,
+          obscureText: true,
+          decoration: InputDecoration(
+            icon: Icon(Icons.lock),
+            hintText: 'Password',
+            labelText: 'Password',
+          ),
+/*              onChanged: (value){
+              },*/
+        ),
+      );
+    });
+  }
+
+  Widget bottonLogin() {
+    return StreamBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      return ElevatedButton(
+        onPressed: () {},
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 17),
+          child: Text('Log in'),
+        ),
+      );
+    });
   }
 }
 
@@ -28,7 +202,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- List<String> items = [];
+  List<String> items = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,16 +213,19 @@ class _MyHomePageState extends State<MyHomePage> {
         padding: const EdgeInsets.all(10),
         itemCount: items.length,
         itemBuilder: (BuildContext context, int index) {
-          return Card(child:ListTile(
-              title: Text(items[index]),
-              subtitle: Text("Galarga"),
-              leading: Image(image: NetworkImage("https://images.unsplash.com/photo-1547721064-da6cfb341d50")),
-              trailing: Icon(Icons.chat)));
+          return Card(
+              child: ListTile(
+                  title: Text(items[index]),
+                  subtitle: Text("Galarga"),
+                  leading: Image(
+                      image: NetworkImage(
+                          "https://images.unsplash.com/photo-1547721064-da6cfb341d50")),
+                  trailing: Icon(Icons.chat)));
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          setState((){
+        onPressed: () {
+          setState(() {
             items.add('raul');
           });
         },
