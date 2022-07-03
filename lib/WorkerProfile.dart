134f69f6a8da99ac7b61462c3e250e8a0bf83019
FreyNell <freybalaguera@gmail.com>
2022-07-02 22:09:44 -0500

    added paddings and aligments to the main column inside the scaffold.

--- lib/WorkerProfile.dart
+++ lib/WorkerProfile.dart
@@ -14,7 +14,8 @@ Widget buildCard({String? path, String? selectedPath}) {
     height: 200,
     color: Colors.black87,
     child: Column(
-      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.max,
       children: [
         if (path != null)
           ClipRect(
@@ -32,107 +33,153 @@ Widget buildCard({String? path, String? selectedPath}) {
 class _WorkerProfile extends State<WorkerProfile> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Beard&Care',
-          style: TextStyle(color: Colors.amber),
-        ),
-        backgroundColor: Colors.black87,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/LoginClient');
-          },
-          icon: const Icon(
-            Icons.arrow_back,
-            color: Colors.amber,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Beard&Care',
+            style: TextStyle(color: Colors.amber),
           ),
-        ),
-      ),
-      body: Column(
-        children: [
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                buildCard(
-                    path:
-                        'https://images.unsplash.com/photo-1654789460791-74c79093c37e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
-                const SizedBox(
-                  width: 4,
-                ),
-                buildCard(),
-                const SizedBox(width: 4),
-                buildCard(),
-                const SizedBox(width: 4),
-                buildCard(),
-                const SizedBox(width: 4)
-              ],
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/LoginClient');
+            },
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.amber,
             ),
           ),
-          const SizedBox(height: 20),
-          Row(
-            children: [
-              Text('Danny Peluquero',
-                  style: TextStyle(
-                      fontFamily: 'CrimsonText',
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24)),
-            ],
-          ),
-          Row(
-            children: [
-              const ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                child: Image(
-                  image: AssetImage('Assets/LogoBeard.png'),
-                  height: 150,
-                  width: 150,
-                  fit: BoxFit.cover,
-                ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  buildCard(
+                      path:
+                          'https://images.unsplash.com/photo-1654789460791-74c79093c37e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
+                  const SizedBox(
+                    width: 4,
+                  ),
+                  buildCard(),
+                  const SizedBox(width: 4),
+                  buildCard(),
+                  const SizedBox(width: 4),
+                  buildCard(),
+                  const SizedBox(width: 4)
+                ],
               ),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 7, vertical: 5),
-                decoration: BoxDecoration(border: Border.all(width: 0.5)),
-                height: 150,
-                width: 255,
-                child: Text('Agregar la descripcion recibida en el worker_form',
-                    style: TextStyle(fontSize: 16, fontFamily: 'CrimsonText')),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          SafeArea(
-            child: Center(
-              child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 7, vertical: 5),
-                  decoration: BoxDecoration(border: Border.all(width: 0.5)),
-                  height: 330,
-                  width: 400,
-                  child: Text(
-                      'Agregar los precios referenciados del worker_screen_form')),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              InkWell(
-                child: TextButton(
-                  onPressed: () {},
-                  child: Text('Cotizar',style: TextStyle(fontSize: 18, fontFamily: 'CrimsonText',color: Colors.black87,fontWeight: FontWeight.bold),),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 15),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text('Danny Peluquero',
+                      style: TextStyle(
+                          fontFamily: 'CrimsonText',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24)),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    child: Image(
+                      image: AssetImage('Assets/LogoBeard.png'),
+                      height: 150,
+                      width: 150,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 7, vertical: 5),
+                      decoration: BoxDecoration(border: Border.all(width: 0.5)),
+                      height: 150,
+                      width: 255,
+                      child: Text(
+                          'Agregar la descripcion recibida en el worker_form',
+                          style: TextStyle(
+                              fontSize: 16, fontFamily: 'CrimsonText')),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10),
+              child: IntrinsicHeight(
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                        child: Container(
+                            padding:
+                                EdgeInsets.symmetric(horizontal: 7, vertical: 5),
+                            decoration:
+                                BoxDecoration(border: Border.all(width: 0.5)),
+                            child: Text(
+                                'Agregar los precios referenciados del worker_screen_form'))),
+                  ],
                 ),
               ),
-              InkWell(
-                child: TextButton(
-                  onPressed: () {},
-                  child: Text('Pagar',style: TextStyle(fontSize: 18, fontFamily: 'CrimsonText',color: Colors.black87,fontWeight: FontWeight.bold),),
-                ),
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  InkWell(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Cotizar',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontFamily: 'CrimsonText',
+                            color: Colors.black87,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Pagar',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontFamily: 'CrimsonText',
+                            color: Colors.black87,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:beardcare/WorkerProfile.dart';
 import 'package:flutter/material.dart';
 import 'ChooseScreen.dart';
 import 'LoginScreen.dart';
+import 'barber_list.dart';
+import 'WorkerProfile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,56 +19,15 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.amber,
         //colorScheme: ColorScheme(primary: ,secondary: ,error: ,background: ,brightness: ,onPrimary: ,onSecondary: ,onSurface: ,onError: ),
       ),
-      initialRoute: '/WorkerProfile',
+      initialRoute: '/',
       routes: {
         '/WorkerProfile': (context) => WorkerProfile(),
         '/LoginClient': (context) => LoginScreen(userType: 'Client',),
         '/LoginBarber': (context) => LoginScreen(userType: 'Barber',),
-        '/': (context) => ChooseScreen()
+        '/Choose': (context) => ChooseScreen(),
+        '/BaberList': (context) => BarberListPage(),
+        '/': (context) => WorkerProfile(),
       },
-    );
-  }
-}
-
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> items = [];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: items.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-              child: ListTile(
-                  title: Text(items[index]),
-                  subtitle: Text("Galarga"),
-                  leading: Image(
-                      image: NetworkImage(
-                          "https://images.unsplash.com/photo-1547721064-da6cfb341d50")),
-                  trailing: Icon(Icons.chat)));
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            items.add('raul');
-          });
-        },
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
